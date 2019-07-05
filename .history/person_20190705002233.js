c917c4ea98ada6dfc5b338f18098d521a60367e5
Piotr9 <33264584+Piotr9@users.noreply.github.com>
2019-07-05 04:20:02 +0200

    First commit

---
+++ .history/person_20190705002233.js
@@ -0,0 +1,6 @@
+const person = {
+    name: 'Jon Doe',
+    age: 30
+}
+
+module.exports = person;
---
+++ .history/reference/fs_demo_20190705033106.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+//Create folder
+// fs.mkdir(path.join(__dirname, '/test'), {}, err => {
+//     if (err) throw err;
+//     console.log('Folder created...');
+// });
+
+//Create and write to file
+fs.writeFile(
+    path.join(__dirname, '/test', 'hello.txt'),
+    'Hello World!',
+    err => {
+        if (err) throw err;
+        console.log('File written to...');
+            //To append file
+        fs.appendFile(
+            path.join(__dirname, '/test', 'hello.txt'),
+            ' I love node.js!',
+            err => {
+                if (err) throw err;
+                console.log('File written to...');
+            });
+    });
+
+                                // To overwrite file
+// fs.writeFile(
+//     path.join(__dirname, '/test', 'hello.txt'),
+//     ' I love node.js!',
+//     err => {
+//         if (err) throw err;
+//         console.log('File written to...');
+//     });
+
+fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
